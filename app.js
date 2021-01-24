b16166ac93b7b149740db9887400575fd3956879
Michael Vautour <michael.e.vautour@gmail.com>
2021-01-23 23:10:13 -0700

    re-built my new members JS to incorpoate all items below a main div, started styling new members section - need to center image between two grid items

--- app.js
+++ app.js
@@ -55,35 +55,39 @@ const newMembers = () => {
     for (i = 0; i < recentMemberJoins.length; i++) {
         let h3Member = document.getElementsByClassName("newMembers")[0]
         let createDiv = document.createElement("div")
-        let createSecondDiv = document.createElement("div")
-        let createThirdDiv = document.createElement("div")
         let createImg = document.createElement('img')
         let createP = document.createElement('p')
         let createLink = document.createElement('a')
+        let createLastP = document.createElement('p')
         const recentMemberJoin = recentMemberJoins[i]
 
         // creates the elements
         createDiv.classList.add('members-container')
-        createImg.classList.add('profile-image')
+        createImg.classList.add('members-image')
+        createP.classList.add('members-name')
+        createLink.classList.add('members-email')
+        createLastP.classList.add('members-jdate')
+
         createImg.src= recentMemberJoin.img;
-        createSecondDiv.classList.add("members-text")
         createP.textContent = recentMemberJoin.pName
         createLink.textContent = recentMemberJoin.email
-        createLink.href= '#'
-        createThirdDiv.textContent=recentMemberJoin.lastPost
+        createLink.href= `mailto:${recentMemberJoin.email}`
+        createLastP.textContent=recentMemberJoin.lastPost
 
-    
         // inserts the elements into the DOM
-        createDiv.append(createImg);
-        createImg.parentNode.insertBefore(createSecondDiv, createImg.nextSibling);
-        createSecondDiv.append(createP)
-        createSecondDiv.append(createLink)
-        createDiv.append(createThirdDiv)
+        createDiv;
+        // createImg.parentNode.insertBefore(createSecondDiv, createImg.nextSibling);
+        createDiv.append(createImg)
+        createDiv.append(createP)
+        createDiv.append(createLink)
+        createDiv.append(createLastP)
         // below inserts the first index into the dom
         h3Member.parentNode.insertBefore(createDiv, h3Member.nextSibling);
         // below inserts index > 0 into the DOM positioning
         var referenceNode = document.getElementsByClassName('members-container')
         referenceNode[i].parentNode.insertBefore(createDiv, referenceNode[i].nextSibling);
+
+
     }      
 }
 
